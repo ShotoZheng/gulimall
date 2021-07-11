cc20bfde76ebeb9d0e8d2ae4dd066e2d3c3209e0
ShotoZheng <shotozheng@163.com>
2021-07-11 18:22:00 +0800

    测试提交

--- gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplication.java
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 第三方服务
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallThirdPartyApplication {
